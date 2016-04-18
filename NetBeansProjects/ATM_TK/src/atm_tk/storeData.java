e2b314dba5abcac1a1b0cffe83f1121719d7c7cd
Pachara Chokphaisantrakun <jeewsn@gmail.com>
2016-04-18 17:53:24 +0700

    Update storeData.java

--- NetBeansProjects/ATM_TK/src/atm_tk/storeData.java
+++ NetBeansProjects/ATM_TK/src/atm_tk/storeData.java
@@ -10,17 +10,29 @@ public class storeData {
         storeData.balance = balance;
     }
     static String no;
-    public static void stateMe(String no) {
+    public static void stateMe(String no) 
+    {
         storeData.no = no;   
     }
     static String no_each;
-    public static void stateMe_Each(String no_each) {
+    public static void stateMe_Each(String no_each) 
+    {
         storeData.no_each = no_each;   
     }
     static int cnt = 1;
     static String mode, amount;
-    public static void moAndMou(String mode, String amount) {
+    
+    public static void moAndMou(String mode, String amount) 
+    {
         storeData.mode = mode;
         storeData.amount = amount;   
+    
     }
+    static String tranBal,tranID;
+    public static void Trans(String tranid, String tranBal)
+    {
+        storeData.tranID= tranid;
+        storeData.tranBal=tranBal;       
+    }
+   
 }
